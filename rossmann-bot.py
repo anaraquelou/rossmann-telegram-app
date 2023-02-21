283e16b9c6ab635d1a653ba251c221fd9affda30
anaraquelou <anaraquellouzeiro@gmail.com>
2023-02-21 17:22:57 -0300

    os.environ

--- rossmann-bot.py
+++ rossmann-bot.py
@@ -127,5 +127,5 @@ def index():
 		return '<h1> Rossmann Telegram BOT </h1>'
 		
 if __name__ == '__main__':
-	port = os.environment.get( 'PORT', 5000 )
+	port = os.environ.get( 'PORT', 5000 )
 	app.run( host = '0.0.0.0', port=port )
